8930226e8835ae10c283f3a3529435576c0450c8
DiPalmaGiuseppe <g.dipalma6@studenti.unipi.it>
2024-12-21 18:41:15 +0100

    scikit-learn monk tests

--- monk.py
+++ monk.py
@@ -1,42 +1,89 @@
+from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 from monk_dataset import monk_loader
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import accuracy_score
+from scipy.stats import uniform
+from sklearn.model_selection import StratifiedKFold
+import warnings
+import random
+warnings.filterwarnings('ignore', message=".*total space of parameters.*")
 
-def test(id):
-    # Carica i dati con normalizzazione
-    X_train, y_train, X_test, y_test = monk_loader(id, normalize=True)
+def test_random_search(model, param_dist):
+    for i in range(3):
+        X_train, y_train, X_test, y_test = monk_loader(i+1)
 
-    # Inizializza il classificatore MLP
-    mlpc = MLPClassifier(max_iter=1000, random_state=42, early_stopping=True, n_iter_no_change=25)
+        stratified_kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-    # Griglia di parametri
-    param_grid = {
-        'hidden_layer_sizes': [(5,), (10,), (20,), (50,), (100,), (200,)],
-        'activation': ['relu', 'logistic', 'identity', 'tanh'],
-        'solver': ['adam', 'sgd'],
-        'alpha': [0.00001 ,0.0001, 0.001, 0.01, 0.1]
-    }
+        penalty = ['l2', 'l1']
 
-    # Ricerca a griglia
-    grid_search = GridSearchCV(estimator=mlpc, param_grid=param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+        if isinstance(model, LogisticRegression):
+            if random.choice(penalty) == 'l2':
+                param_dist["penalty"] = ['l2']
+                param_dist["solver"] = ['liblinear', 'saga', 'newton-cg', 'lbfgs'] 
+            else:
+                param_dist["penalty"] = penalty
+                param_dist["solver"] = ['liblinear', 'saga']
 
-    grid_search.fit(X_train, y_train)
+        random_search = RandomizedSearchCV(estimator=model, param_distributions=param_dist, 
+                                           n_iter=500, cv=stratified_kfold, scoring='accuracy', 
+                                           random_state=42, n_jobs=-1)
+        
+        random_search.fit(X_train, y_train)
 
-    print("Migliori parametri:", grid_search.best_params_)
+        print("Best Parameters:", random_search.best_params_)
+        
+        best_model = random_search.best_estimator_
 
-    # Punteggi di validazione durante la ricerca a griglia
-    print("Punteggi di validazione durante la ricerca a griglia:")
-    for mean_score, params in zip(grid_search.cv_results_['mean_test_score'], grid_search.cv_results_['params']):
-        print(f"Punteggio: {mean_score:.4f}, Parametri: {params}")
-    
-    best_model = grid_search.best_estimator_
+        # Final prediction on test data
+        y_pred = best_model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print("Test set accuracy:", accuracy)
 
-    # Predizione finale sui dati di test
-    y_pred = best_model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Accuratezza finale sul test dataset:", accuracy)
-    print(f"Punteggio di training migliore: {grid_search.best_score_:.4f}")
+# SVM Classifier
+print("---- SVM ----")
+svm = SVC(random_state=42)
+svm_param_dist = {
+    'C': uniform(0.1, 1000),
+    'kernel': ['linear', 'rbf', 'poly', 'sigmoid'],
+    'gamma': uniform(0.001, 10000)
+}
+test_random_search(svm, svm_param_dist)
 
-for i in range(3):
-    test(i + 1)
+# MLP Classifier
+print("---- MLP Classifier ----")
+mlp = MLPClassifier(random_state=42, max_iter=10000, early_stopping=True)
+mlp_param_dist = {
+    'hidden_layer_sizes': [(1,), (2,), (5,), (10,),(2,2), (5,5), (10,10)],
+    'activation': ['tanh', 'relu', 'logistic'],
+    'solver': ['adam', 'sgd'],
+    'alpha': uniform(0.0001, 0.1),
+    'learning_rate': ['constant', 'invscaling', 'adaptive']
+}
+test_random_search(mlp, mlp_param_dist)
+
+# Logistic Regression
+print("---- Logistic Regression ----")
+logreg = LogisticRegression(random_state=42, max_iter=10000)
+logreg_param_dist = {
+    'C': uniform(0.01, 1000),
+    'tol': uniform(1e-4, 1e-2),
+    'class_weight': [None, 'balanced'],
+    'warm_start': [True, False],
+    'intercept_scaling': uniform(1,1000)
+}
+test_random_search(logreg, logreg_param_dist)
+
+# Decision Tree Classifier
+print("---- Decision Tree ----")
+dt = DecisionTreeClassifier(random_state=42)
+dt_param_dist = {
+    'max_depth': [3, 5, 10, 15, None],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'criterion': ['gini', 'entropy'],
+}
+
+test_random_search(dt, dt_param_dist)
--- monk_dataset.py
+++ monk_dataset.py
@@ -1,20 +1,25 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 
-    
-def monk_loader(id, normalize = False):
-    train = pd.read_csv(f"monk_dataset/monks-{id}.train", sep = ' ', header = None, usecols=range(1, 8))
-    test = pd.read_csv(f"monk_dataset/monks-{id}.test", sep = ' ', header = None, usecols=range(1, 8))
+def monk_loader(id):
+    # Load the training and test data
+    train = pd.read_csv(f"monk_dataset/monks-{id}.train", sep=' ', header=None, usecols=range(1, 8))
+    test = pd.read_csv(f"monk_dataset/monks-{id}.test", sep=' ', header=None, usecols=range(1, 8))
 
     y_train = train.iloc[:, 0]
     X_train = train.iloc[:, 1:]
 
     y_test = test.iloc[:, 0]
     X_test = test.iloc[:, 1:]
-    
-    if normalize:
-        scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
 
-    return X_train, y_train, X_test, y_test
+    encoder = OneHotEncoder(sparse_output = False)
+
+    X_train = pd.DataFrame(encoder.fit_transform(X_train)).astype(int)
+    X_test = pd.DataFrame(encoder.transform(X_test)).astype(int)
+
+    return (
+        X_train,  # One-hot encoded training features as DataFrame
+        y_train,             # Training labels
+        X_test,   # One-hot encoded test features as DataFrame
+        y_test               # Test labels
+    )
